8eb6f06d379f3c7cbc4e56c96fdbae86b22bfd36
MD ALI RAFAT <80602392+mdrafat@users.noreply.github.com>
2021-09-03 10:32:20 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let chkbox = document.querySelector('.chkbox');
+let rafat = document.querySelector('.rafat');
+
+chkbox.addEventListener('click', function (){
+
+    rafat.style.display = 'block';
+
+
+})
